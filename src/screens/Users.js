6423091322d6bdf9bf8b04275c8cf3bf88f6cb17
Nacho Zavala <cionagi@hotmail.cl>
2019-11-30 12:20:52 -0300

    Show post by user + redux

--- src/screens/Users.js
+++ src/screens/Users.js
@@ -1,27 +1,65 @@
 //Dependency
 import React, { Component } from 'react'
-import CardPost from '../components/CardPost'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { callGetUser } from '../store/actions/users'
 
 //Components
+import CardPost from '../components/CardPost'
+import Loading from '../components/Loading'
+
 //Assets
 
 class Users extends Component {
+  state = {
+    isLoading: true,
+    posts: [],
+  }
+
+  componentDidMount() {
+    const userId = this.props.match.params.id
+    this.props.actions.callGetUser(userId)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { users } = this.props
+    if (users.isFetching !== nextProps.users.isFetching && !nextProps.users.isFetching) {
+      this.setState({
+        isLoading: false,
+        posts: nextProps.users.posts,
+      })
+    }
+  }
+
+  renderCardPost() {
+    const { posts } = this.state
+    if (posts !== undefined) {
+      return posts.map((post) => {
+        return <CardPost key={post.id} post={post} />
+      })
+    } else {
+      return <h1>No posts</h1>
+    }
+  }
+
   render() {
+    const { isLoading } = this.state
     return (
       <div className="container">
-        <div className={'row'}>
-          <CardPost />
-          <CardPost />
-          <CardPost />
-          <CardPost />
-          <CardPost />
-          <CardPost />
-          <CardPost />
-          <CardPost />
-        </div>
+        <div className={'row'}>{isLoading ? <Loading /> : this.renderCardPost()}</div>
       </div>
     )
   }
 }
 
-export default Users
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return { actions: bindActionCreators({ callGetUser }, dispatch) }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users)
--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
 import posts from './posts'
+import users from './users'
+
 //reducers
 const AppReducer = combineReducers({
   posts,
+  users,
 })
 export default AppReducer
