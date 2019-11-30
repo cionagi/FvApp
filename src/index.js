8a6e76878498763d74b248835770fc7b889077e2
Nacho Zavala <cionagi@hotmail.cl>
2019-11-30 09:05:06 -0300

    Add Redux in App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+// Dependency
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import * as serviceWorker from './serviceWorker'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import configureStore from './store/configureStore'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Components
+import App from './App'
+
+// Assets
+
+let { store, persistor } = configureStore()
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+)
 
 serviceWorker.unregister()
