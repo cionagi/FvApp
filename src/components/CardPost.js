9b2b764ea7bf12538da1a8c6f75e06646f6c854c
Nacho Zavala <cionagi@hotmail.cl>
2019-11-30 13:27:37 -0300

    Update style CardPost

--- src/components/CardPost.js
+++ src/components/CardPost.js
@@ -12,14 +12,16 @@ class CardPost extends Component {
     } = this.props
 
     return (
-      <div className={'col-3'}>
+      <div className={'col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3'}>
         <div className={'m-2 '}>
           <Link to={`/posts/${id}`}>
-            <div className="card shadow-sm">
+            <div className="card shadow-sm custom-card">
               <img src={'https://via.placeholder.com/450x300'} className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{body}</p>
+                <h5 className="card-title text-truncate font-weight-bold text-muted">{title}</h5>
+                <p className="card-text text-muted">{body}</p>
+              </div>
+              <div className="card-body">
                 {showAuthor && (
                   <Link to={`/users/${userId}`}>
                     <span className="badge badge-secondary">Autor</span>
