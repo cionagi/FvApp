e2894cb239324e5e65003927b7e4f3f5b7bc86e9
Nacho Zavala <cionagi@hotmail.cl>
2019-11-29 23:06:08 -0300

    Add card posts

---
+++ src/components/CardPost.js
@@ -0,0 +1,16 @@
+//Dependency
+import React, { Component } from 'react'
+
+//Components
+//Assets
+class CardPost extends Component {
+  render() {
+    return (
+      <>
+        <h1>INDEX</h1>
+      </>
+    )
+  }
+}
+
+export default CardPost
