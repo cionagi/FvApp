c127308456c173ca5aee7d29249b7fc954564a9a
Nacho Zavala <cionagi@hotmail.cl>
2019-11-30 00:04:21 -0300

    Add components post and comments

--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -1,5 +1,6 @@
 //Dependency
 import React, { Component } from 'react'
+import ComentPost from '../components/ComentPost'
 
 //Components
 //Assets
@@ -10,8 +11,34 @@ class Posts extends Component {
       <>
         <div className="jumbotron jumbotron-fluid">
           <div className="container">
-            <h1 className="display-4">Fluid jumbotron</h1>
-            <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            <img src={'https://via.placeholder.com/1000x300'} />
+          </div>
+        </div>
+        <div className={'container'}>
+          <div className={'col-12 text-center'}>
+            <h1>TITULO</h1>
+            <span className="badge badge-secondary">Autor</span>
+          </div>
+          <div className={'col-12'}>
+            <p className={'my-5 text-justify'}>
+              lLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+              since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+              five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+              release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+              including versions of Lorem Ipsum.
+            </p>
+          </div>
+          <div className={'col-12'}>
+            <h6>Comentarios</h6>
+            <ComentPost />
+            <ComentPost />
+            <ComentPost />
+            <ComentPost />
+            <ComentPost />
+            <ComentPost />
+            <ComentPost />
+            <ComentPost />
+            <ComentPost />
           </div>
         </div>
       </>
